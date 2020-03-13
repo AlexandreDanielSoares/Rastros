6b9309cf496aa8815dee05d84a2c5f122fde30d1
Pedro Alexandre Sousa <61886466+PWACN@users.noreply.github.com>
2020-03-13 18:02:36 +0000

    Update RastrosLogic.c

--- RastrosLogic.c
+++ RastrosLogic.c
@@ -10,13 +10,13 @@ int jogar (ESTADO *e, COORDENADA c){
 		(e -> tab[c.linha][c.coluna] == PRETA )) return 0;
     else {
        if (e->jogador_atual == 2){
-	       // Jogada anterior Caso seja jogador 2
-       		e->jogador_atual = 1;
-       		e->num_jogadas ++;
+	  // Jogada anterior Caso seja jogador 2
+          e->jogador_atual = 1;
+       	  e->num_jogadas ++;
        }
        else{
-	// Jogada anterior Caso seja jogador 1
-       	e->jogador_atual ++;
+	   // Jogada anterior Caso seja jogador 1
+       	   e->jogador_atual ++;
        }
        e->tab[x][y] = PRETA;
        e->tab[c.linha][c.coluna] = BRANCA;
